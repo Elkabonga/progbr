4f7a023754fc3a37f14b67f4da263accec41bd67
Tiago Santos <tisgo.slip@hotmail.com>
2021-03-22 23:26:56 -0300

    wip reactJs

---
+++ views/10todo/src/Todo.js
@@ -0,0 +1,36 @@
+import React, { useState } from "react";
+import List from './components/List'
+import TodoForm from './components/TodoForm'
+import Item from './utils/Item'
+import "./Todo.css";
+
+function Todo() {
+
+  const [items, setItems] = useState([])
+
+  function onAddItem(text) {
+
+    let it = new Item(text)
+
+    setItems([...items, it])
+  }
+
+  function onItemDeleted(item) {
+    let filteredItems = items.filter(it => it.id != item.id)
+
+    setItems(filteredItems)
+  }
+
+  return (
+    <div className="container">
+      <h1>Todo</h1>
+
+      <TodoForm onAddItem={onAddItem}></TodoForm>
+
+      <List onItemDeleted={onItemDeleted} items={items}></List>
+
+    </div>
+  );
+}
+
+export default Todo;
