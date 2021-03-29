2ccc3328279810f6eb47749e90897cfcb58a4c05
Tiago Santos <tisgo.slip@hotmail.com>
2021-03-29 09:37:09 -0300

    wip redux

--- Redux/index.js
+++ Redux/index.js
@@ -1,10 +1,11 @@
 const redux = require('redux')
 const createStore = redux.createStore
+const combineReducer = redux.combineReducers
 
 const incrementAction = (value) => { return { type: 'INCREMENT', payload: value || 1 } }
 const decrementAction = (value) => { return { type: 'DECREMENT', payload: value || 1 } }
 
-function counterReducer(state = 0, action) {
+function counterReducer(state = 6, action) {
 
   switch (action.type) {
     case 'INCREMENT':
@@ -18,14 +19,29 @@ function counterReducer(state = 0, action) {
 
 }
 
-const store = createStore(counterReducer)
+const addItemAction = (item) => { return { type: 'ADD_ITEM', payload: item } }
 
+const listReducer = (state = ['Item Padrão'], action) => {
+
+  switch (action.type) {
+    case 'ADD_ITEM':
+      return [...state, action.payload]
+    default:
+      return state
+  }
+}
+// ___________________________
+const allReducers = combineReducer({
+  counter: counterReducer,
+  list: listReducer
+})
+
+const store = createStore(allReducers)
 console.log(store.getState())
 
 store.subscribe(() => { console.log(store.getState()); })
-
-store.dispatch(incrementAction(10))
+store.dispatch(addItemAction('Novo item'))
 store.dispatch(incrementAction())
 store.dispatch(incrementAction())
-store.dispatch(decrementAction())
+store.dispatch(incrementAction(5))
 store.dispatch(decrementAction())
