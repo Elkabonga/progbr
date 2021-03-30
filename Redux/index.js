20af7f0fd1bf398fc7321e18e922a8d0e156ef13
Tiago Santos <tisgo.slip@hotmail.com>
2021-03-30 11:00:13 -0300

    wip redux

--- Redux/index.js
+++ Redux/index.js
@@ -1,36 +1,12 @@
 const redux = require('redux')
+const { incrementAction, decrementAction } = require('./actions/CounterActions')
+const { addItemAction } = require('./actions/ListActions')
+const counterReducer = require('./reducers/CounterReducer')
+const listReducer = require('./reducers/ListReducer')
+
 const createStore = redux.createStore
 const combineReducer = redux.combineReducers
 
-const incrementAction = (value) => { return { type: 'INCREMENT', payload: value || 1 } }
-const decrementAction = (value) => { return { type: 'DECREMENT', payload: value || 1 } }
-
-function counterReducer(state = 6, action) {
-
-  switch (action.type) {
-    case 'INCREMENT':
-      return state + action.payload
-    case 'DECREMENT':
-      return state - action.payload
-
-    default:
-      return state
-  }
-
-}
-
-const addItemAction = (item) => { return { type: 'ADD_ITEM', payload: item } }
-
-const listReducer = (state = ['Item Padrão'], action) => {
-
-  switch (action.type) {
-    case 'ADD_ITEM':
-      return [...state, action.payload]
-    default:
-      return state
-  }
-}
-// ___________________________
 const allReducers = combineReducer({
   counter: counterReducer,
   list: listReducer
